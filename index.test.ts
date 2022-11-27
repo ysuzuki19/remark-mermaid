56487a4ec195089dd774d3fc6e55c203f9f54821
ysuzuki19 <ysuzuki.cpp@gmail.com>
2022-11-27 21:07:28 +0900

    add sample test

--- index.test.ts
+++ index.test.ts
@@ -1,3 +1,9 @@
-test('sample', () => {
-  expect(1).toBe(1);
+import { remark } from 'remark';
+import remarkMermaid from '.';
+
+test('sample', async () => {
+  const res = await remark().use(remarkMermaid).process('<html></html>');
+  console.log(res);
 });
+
+export default {};
