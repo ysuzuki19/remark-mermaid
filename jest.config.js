61fe86d78c1b603d823e241a62a7c40db354514c
ysuzuki19 <ysuzuki.cpp@gmail.com>
2026-03-10 02:35:15 +0900

    migrate rollup to tsdown

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(remark|unified|bail|devlop|is-plain-obj|trough|vfile|unist-util-stringify-position|mdast-util-from-markdown|mdast-util-to-markdown|mdast-util-to-string|mdast-util-phrasing|micromark|micromark-.*|decode-named-character-reference|character-entities|longest-streak|zwitch|unist-util-visit|unist-util-is)/)',
+  ],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
